d0473ab6e0efb26eb52f8afa3de4b9e5db92996b
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2021-11-15 16:24:17 -0300

    Requirement 4: Tests the <NotFount /> component

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Testes para o componente <NotFound.js />', () => {
+  beforeEach(() => {
+    renderWithRouter(<NotFound />);
+  });
+  it('Testa se a página tem um componente h2 com o texto "pageRequested not found"',
+    () => {
+      const notFoundH2 = screen.getByRole('heading',
+        {
+          name: /Page requested not found/i,
+          level: 2,
+        });
+      expect(notFoundH2).toBeInTheDocument();
+    });
+
+  it('Testa se a página renderiza o gif selecionado', () => {
+    const url = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const gif = screen
+      .getByAltText('Pikachu crying because the page requested was not found');
+
+    expect(gif.src).toContain(url);
+  });
+});
