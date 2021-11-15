eed4a40fa79aa139376ada3073943a6208333fb2
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2021-11-15 18:09:45 -0300

    Requirement 5: Testas the  <Pokedex /> component

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,73 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Teste o componente <Pokedex.js />', () => {
+  beforeEach(() => {
+    renderWithRouter(<App />);
+  });
+  it('Testa se página contém um h2 com o texto "Encountered pokémons".', () => {
+    const pokedexH2 = screen.getByRole('heading', {
+      level: 2,
+      name: /Encountered pokémons/i,
+    });
+
+    expect(pokedexH2).toBeInTheDocument();
+  });
+
+  it('Testa se o próximo pokémon é mostrado após o click no botão.', () => {
+    const nextButton = screen.getByText(/próximo pokémon/i);
+    expect(nextButton).toBeInTheDocument();
+    userEvent.click(nextButton);
+
+    const imgPokemon = screen.getByRole('img', {
+      name: /charmander sprite/i,
+    });
+
+    expect(imgPokemon).toBeInTheDocument();
+  });
+
+  it('Testa se é mostrado apenas um Pokémon por vez.', () => {
+    const pokemonClicado = screen.getByText(/pikachu/i);
+
+    expect(pokemonClicado).toBeInTheDocument();
+
+    const pokemons = [
+      'Charmander',
+      'Caterpie',
+      'Ekans',
+      'Alakazam',
+      'Mew',
+      'Rapidash',
+      'Snorlax',
+      'Dragonair',
+    ];
+
+    pokemons.forEach((pokemon) => {
+      const poke = screen.queryByText(pokemon);
+
+      expect(poke).not.toBeInTheDocument();
+    });
+  });
+
+  it('Teste se a Pokédex tem os botões de filtro.', () => {
+    const filterButton = screen.getAllByTestId('pokemon-type-button');
+    const types = ['Electric', 'Fire', 'Bug', 'Poison', 'Psychic', 'Normal', 'Dragon'];
+
+    expect(filterButton.length).toBe(types.length);
+
+    filterButton.forEach((buttonEl, i) => expect(buttonEl).toHaveTextContent(types[i]));
+  });
+
+  it('Teste se a Pokédex contém um botão para resetar o filtro', () => {
+    const buttonReset = screen.getByRole('button', {
+      name: /all/i,
+    });
+
+    userEvent.click(buttonReset);
+
+    expect(buttonReset).toBeInTheDocument();
+  });
+});
