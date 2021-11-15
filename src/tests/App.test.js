fdf68162ff0a99b25671deed1336be23d4c62606
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2021-11-15 14:24:08 -0300

    Requirement 1: Tests the <App /> component

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Testes para o componente <App.js />', () => {
+  it('Testa se o primeiro link tem o texto Home', () => {
+    renderWithRouter(<App />);
+
+    const testLinkToHome = screen.getByRole('link', { name: 'Home' });
+
+    expect(testLinkToHome).toBeInTheDocument();
+  });
+
+  it('Testa se o segundo link tem o texto Home', () => {
+    renderWithRouter(<App />);
+
+    const testLinkToAbout = screen.getByRole('link', { name: 'About' });
+
+    expect(testLinkToAbout).toBeInTheDocument();
+  });
+
+  it('Testa se o terceiro link tem o texto Home', () => {
+    renderWithRouter(<App />);
+
+    const testLinkToFavoritePokemons = screen
+      .getByRole('link', { name: 'Favorite Pokémons' });
+
+    expect(testLinkToFavoritePokemons).toBeInTheDocument();
+  });
+});
