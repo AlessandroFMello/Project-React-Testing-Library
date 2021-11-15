4605e1cc7bf33b6d8061065b8a54c5a389bbb5d3
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2021-11-15 15:49:51 -0300

    Requirement 3: Tests the <FavoritePokemons /> component

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Testes para o componente <FavoritePokemons.js />', () => {
+  it('Exibe mensagem de erro quando não há pokémons favoritos', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const notFound = screen.getByText(/No favorite pokemon found/i);
+
+    expect(notFound).toBeInTheDocument();
+  });
+});
