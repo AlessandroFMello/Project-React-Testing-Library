01d6cc2548a61c9852123ab227fd6be873806ac2
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2021-11-16 07:54:38 -0300

    refatora o codigo

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -9,52 +9,52 @@ describe('Testa o componente <Pokémon />', () => {
     renderWithRouter(<App />);
   });
 
-  const nextPoke = 'Próximo pokémon';
+  const nextPokemon = 'Próximo pokémon';
   it('Testa se o nome correto do pokemon é exibido', () => {
     const pokemonName = screen.getByTestId('pokemon-name');
     expect(pokemonName).toHaveTextContent('Pikachu');
-    const nxtBtn = screen.getByRole('button', { name: nextPoke });
-    userEvent.click(nxtBtn);
-    const nxtName = screen.getByTestId('pokemon-name');
-    expect(nxtName).toHaveTextContent('Charmander');
+    const nextButton = screen.getByRole('button', { name: nextPokemon });
+    userEvent.click(nextButton);
+    const nextPokemonName = screen.getByTestId('pokemon-name');
+    expect(nextPokemonName).toHaveTextContent('Charmander');
   });
 
   it('Testa se o tipo correto do pokemon é exibido', () => {
     const pokemonType = screen.getByTestId('pokemon-type');
     expect(pokemonType).toHaveTextContent('Electric');
-    const dragonFilter = screen.getByRole('button', { name: 'Dragon' });
-    userEvent.click(dragonFilter);
+    const dragonTypeFilter = screen.getByRole('button', { name: 'Dragon' });
+    userEvent.click(dragonTypeFilter);
     const type = screen.getByTestId('pokemon-type');
     expect(type).toHaveTextContent('Dragon');
   });
 
   it('Testa se o peso médio do pokemon é exibido', () => {
-    const peso = screen.getByTestId('pokemon-weight');
-    expect(peso).toHaveTextContent('Average weight: 6.0 kg');
-    const nxtBtn = screen.getByRole('button', { name: nextPoke });
-    userEvent.click(nxtBtn);
-    const nxtPeso = screen.getByTestId('pokemon-weight');
-    expect(nxtPeso).toHaveTextContent('Average weight: 8.5 kg');
+    const pokemonWeight = screen.getByTestId('pokemon-weight');
+    expect(pokemonWeight).toHaveTextContent('Average weight: 6.0 kg');
+    const nextButton = screen.getByRole('button', { name: nextPokemon });
+    userEvent.click(nextButton);
+    const nextWeight = screen.getByTestId('pokemon-weight');
+    expect(nextWeight).toHaveTextContent('Average weight: 8.5 kg');
   });
 
   it('Testa se a imagem do pokemon é exibida', () => {
-    const pokeImg = screen.getByAltText(/Pikachu sprite/i);
-    expect(pokeImg).toBeInTheDocument();
-    expect(pokeImg).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
-    expect(pokeImg).toHaveAttribute('alt', 'Pikachu sprite');
-    const nxtBtn = screen.getByRole('button', { name: nextPoke });
-    userEvent.click(nxtBtn);
-    const nxtPoke = screen.getByRole('img');
-    expect(nxtPoke).toBeInTheDocument();
-    expect(nxtPoke).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/0/0a/Spr_5b_004.png');
-    expect(nxtPoke).toHaveAttribute('alt', 'Charmander sprite');
+    const pokemonImage = screen.getByAltText(/Pikachu sprite/i);
+    expect(pokemonImage).toBeInTheDocument();
+    expect(pokemonImage).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+    expect(pokemonImage).toHaveAttribute('alt', 'Pikachu sprite');
+    const nextButton = screen.getByRole('button', { name: nextPokemon });
+    userEvent.click(nextButton);
+    const nxtPokemon = screen.getByRole('img');
+    expect(nxtPokemon).toBeInTheDocument();
+    expect(nxtPokemon).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/0/0a/Spr_5b_004.png');
+    expect(nxtPokemon).toHaveAttribute('alt', 'Charmander sprite');
   });
 
   it('Testa se o card tem um link de navegação para mais detalhes', () => {
     const link = screen.getByRole('link', { name: /more details/i });
     expect(link).toHaveAttribute('href', '/pokemons/25');
-    const nxtBtn = screen.getByRole('button', { name: nextPoke });
-    userEvent.click(nxtBtn);
+    const nextButton = screen.getByRole('button', { name: nextPokemon });
+    userEvent.click(nextButton);
     const nxtLink = screen.getByRole('link', { name: /more details/i });
     expect(nxtLink).toHaveAttribute('href', '/pokemons/4');
   });
